05f591941a8524310053a4971f037fd6ac9b2128
backurs <arturs.backurs@gmail.com>
2021-10-06 12:09:38 -0700

    nvidia_docker

--- experiments/nvidia_docker.sh
+++ experiments/nvidia_docker.sh
@@ -4,6 +4,7 @@ nvidia-docker run -it \
 -v '/home/abackurs/GitHub/NLP:/workspace/NLP' \
 -v '/home/abackurs/data:/workspace/data' \
 --rm \
---name test \
+-d \
+--name test_0 \
 exp_nvidia \
 python /workspace/NLP/train.py
